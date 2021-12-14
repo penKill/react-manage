c99331afde89b5f5ef16801c133082ee952f000c
王碧林 <784690321@qq.com>
2021-12-14 17:20:21 +0800

    增加ant的样式引入

---
+++ src/pages/Login.tsx
@@ -0,0 +1,60 @@
+import React, {Component, createRef, RefObject} from "react";
+import {Form, FormInstance, Checkbox,Button,Input} from "antd";
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
+
+export default class Login extends Component<any, any> {
+    fromRef: RefObject<FormInstance>
+
+
+    constructor(props: any, context: any) {
+        super(props, context);
+        this.fromRef = createRef<FormInstance>()
+    }
+
+    render() {
+        return (
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                initialValues={{ remember: true }}
+                // onFinish={onFinish}
+                // onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
